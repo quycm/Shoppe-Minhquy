c711124b018940eb3d1e157ee019664a285f6d9a
quy chu <quycm2802@gmail.com>
2020-09-23 16:07:03 +0700

    Updates

---
+++ precache-manifest.91f9c56ec40f107133c2fe56378e6eab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fa619e565cb02b9bd8fe9522df63d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62e0076a898f34b03b32",
+    "url": "/static/css/2.c9a2178a.chunk.css"
+  },
+  {
+    "revision": "913f73e42e552287aa57",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "62e0076a898f34b03b32",
+    "url": "/static/js/2.63f7a9f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.63f7a9f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913f73e42e552287aa57",
+    "url": "/static/js/main.1b92dfb0.chunk.js"
+  },
+  {
+    "revision": "31de383a84f0b531942d",
+    "url": "/static/js/runtime-main.408e7f6b.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
